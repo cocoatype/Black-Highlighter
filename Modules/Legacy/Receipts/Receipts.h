398c18e90f801a672c2b29cf0a31699c8d7e479e
Geoff Pado <geoff@pado.name>
2024-04-29 19:49:45 -0700

    Fix OpenSSL embed

--- Modules/Legacy/Receipts/Receipts.h
+++ Modules/Legacy/Receipts/Receipts.h
@@ -7,20 +7,6 @@
 //
 
 #import <Foundation/Foundation.h>
-//#include <openssl/conf.h>
-//#include <openssl/evp.h>
-//#include <openssl/err.h>
-//#include <openssl/bio.h>
-//#include <openssl/ssl.h>
-//#include <openssl/md4.h>
-//#include <openssl/md5.h>
-//#include <openssl/sha.h>
-//#include <openssl/hmac.h>
-//#include <openssl/rand.h>
-//#include <openssl/ripemd.h>
-//#include <openssl/pkcs12.h>
-//#include <openssl/x509v3.h>
-
 
 //! Project version number for Receipts.
 FOUNDATION_EXPORT double ReceiptsVersionNumber;
@@ -29,5 +15,3 @@ FOUNDATION_EXPORT double ReceiptsVersionNumber;
 FOUNDATION_EXPORT const unsigned char ReceiptsVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <Receipts/PublicHeader.h>
-
-
