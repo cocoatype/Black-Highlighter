bd18fda2a0a911902ed319f285cff49727cb9c20
Geoff Pado <geoff@pado.name>
2024-05-19 06:42:42 -0700

    Link TelemetryClient to tests

--- Tuist/ProjectDescriptionHelpers/Targets/Capabilities/Logging.swift
+++ Tuist/ProjectDescriptionHelpers/Targets/Capabilities/Logging.swift
@@ -11,7 +11,12 @@ public enum Logging {
         )
     }
 
-    public static let testTarget = Target.capabilitiesTestTarget(name: "Logging")
+    public static let testTarget = Target.capabilitiesTestTarget(
+        name: "Logging",
+        dependencies: [
+            .external(name: "TelemetryClient"),
+        ]
+    )
 
     public static let doublesTarget = Target.capabilitiesDoublesTarget(name: "Logging")
 }
