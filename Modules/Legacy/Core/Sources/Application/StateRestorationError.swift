331d8cbb6a2e02ad9fb948bf4669c66b1863c58d
Geoff Pado <geoff@pado.name>
2024-06-24 21:57:19 -0700

    Remove RestorationImageCache

---
+++ Modules/Legacy/Core/Sources/Application/StateRestorationError.swift
@@ -0,0 +1,6 @@
+//  Created by Geoff Pado on 6/24/24.
+//  Copyright © 2024 Cocoatype, LLC. All rights reserved.
+
+enum StateRestorationError: Error {
+    case invalidImageData
+}
---
+++ Modules/Legacy/Editing/Sources/FileURLProvider.swift
@@ -0,0 +1,24 @@
+//  Created by Geoff Pado on 5/1/21.
+//  Copyright © 2021 Cocoatype, LLC. All rights reserved.
+
+import ErrorHandling
+import UIKit
+
+public protocol FileURLProvider {
+    var representedFileURL: URL? { get }
+    func updateRepresentedFileURL(to newURL: URL)
+}
+
+extension FileURLProvider {
+    var representedFileName: String? { representedFileURL?.lastPathComponent }
+}
+
+public extension UIResponder {
+    var fileURLProvider: FileURLProvider? {
+        if let provider = (self as? FileURLProvider) {
+            return provider
+        }
+
+        return next?.fileURLProvider
+    }
+}
